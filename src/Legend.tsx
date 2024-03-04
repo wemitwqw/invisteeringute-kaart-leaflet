d06b43c3feeeafafc12b9c3a381e5dac7731de84
Vladislav Priske <Vladislav.Priske@energia.ee>
2024-03-04 12:52:06 +0200

    upd

--- src/Legend.tsx
+++
@@ -1,9 +0,0 @@
-function MapLegendControl() {
-    return (
-        <div className='leaflet-bottom leaflet-left leaflet-control leaflet-bar map-legend'>
-            <button>Legend</button>
-        </div>
-    )
-}
-
-export default MapLegendControl;
